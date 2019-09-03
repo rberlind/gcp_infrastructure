fe8b2296fbceeb7154419deb24ba246b9a94511d
Roger Berlind <roger@hashicorp.com>
2019-09-03 11:27:49 -0400

    fix source_ranges

--- main.tf
+++ main.tf
@@ -58,8 +58,7 @@ module "network_firewall" {
   network = "${module.network.name}"
   ports = [80]
   protocol = "TCP"
-  #source_ranges = ["0.0.0.0/0"]
-  source_ranges = "["0.0.0.0/0"]"
+  source_ranges = ["0.0.0.0/0"]
 }
 
 module "network_subnet" {
